f25c8d387899bc2524f2fd1bb1c19060f5343e53
Phil Wilkes <p.wilkes@ucl.ac.uk>
2016-04-26 12:03:19 +0100

    renamed

--- hips.py
+++ hips.py
@@ -2,22 +2,49 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-def hips2img(file, order=[0,1,2], stretch=True, imshow=True, 
-             image_save=False, ax=None):
-    
+
+def read_header(fname):
+
+    """
+    :param fname:
+    :return:
+
+    header_length, bands, res_x, res_y, fmt
+    """
+
     # grab header info
-    header_length = open(file).read().find('\n.')
-    header = open(file).read()[:header_length].split()
+    header_length = open(fname).read().find('\n.')
+    header = open(fname).read()[:header_length].split()
     bands = int(header[1])
     res_x = int(header[2])
     res_y = int(header[3])
     fmt = int(header[4])
-    
+
+    return header_length, bands, res_x, res_y, fmt
+
+def read_hips(fname):
+
+    header_length, bands, res_x, res_y, fmt = read_header(fname)
+
     # extract image from hips
-    hips = np.fromfile(file, np.float32)
+    hips = np.fromfile(fname, np.float32)
     hips_length = len(hips)
     img = hips[hips_length - (bands * res_x * res_y):].reshape(bands, res_x, res_y)
     img = np.rot90(img.T, 3)
+
+    return img, bands, res_x, res_y, fmt
+
+
+def hipstats(fname):
+
+    img, bands, res_x, res_y, fmt = read_hips(fname)
+    return img.min(), img.max(), img.mean(), img.std()
+
+
+def hips2img(fname, order=[0,1,2], stretch=True, imshow=True,
+             image_save=False, ax=None):
+    
+    img, bands, res_x, res_y, fmt = read_hips(fname)
     
     #stretch
     for b in range(bands):
@@ -42,7 +69,7 @@ def hips2img(file, order=[0,1,2], stretch=True, imshow=True,
 
     # save image
     if image_save:
-        plt.imsave(os.path.splitext(file)[0] + '.png', img)
+        plt.imsave(os.path.splitext(fname)[0] + '.png', img)
 
     # plot image to screen
     if imshow:
