dbab735cdfabdce6f4e0f3c41de3a1fb4a32b531
Phil Wilkes <p.wilkes@ucl.ac.uk>
2016-04-28 12:08:39 +0100

    replaced tabs with spaces

--- run_start.py
+++ run_start.py
@@ -1,27 +1,27 @@
 import subprocess
 
+
 def run_start(cmd):
-	cmd, start = cmd.split('|')
-	if not start.split()[-1].endswith('.obj'):
-		raise IOError('.obj needs to be the last command')
-	ps = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-	start_process = subprocess.Popen(start.split(), 
-									 stdin=ps.stdout, 
-									 stderr=subprocess.PIPE)
-	
-	error = None
-	for line in start_process.stderr:
-		print line
-		if line.startswith('rat'):
-			error = True
-		elif line.startswith('error'):
-			error = True
-		elif "multiple defined material" in line:
-			error = True
-		elif "error defining number of pulse samples" in line:
-			error = True
-		elif "not found for cloning" in line:
-			error = True 
+    cmd, start = cmd.split('|')
+    if not start.split()[-1].endswith('.obj'):
+        raise IOError('.obj needs to be the last command')
+    ps = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    start_process = subprocess.Popen(start.split(),
+                                     stdin=ps.stdout,
+                                     stderr=subprocess.PIPE)
 
-	return error
+    error = None
+    for line in start_process.stderr:
+        print line
+        if line.startswith('rat'):
+            error = True
+        elif line.startswith('error'):
+            error = True
+        elif "multiple defined material" in line:
+            error = True
+        elif "error defining number of pulse samples" in line:
+            error = True
+        elif "not found for cloning" in line:
+            error = True
 
+    return error
